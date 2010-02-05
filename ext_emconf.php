a1b68354c7e24a1413d555432b7cdd1a3808eba1
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-05 16:59:20 +0000

    Initial code from Kickstarter
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/examples/trunk@29717 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "examples".
+#
+# Auto generated 05-02-2010 16:53
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Code examples from Core Documentation',
+	'description' => 'This extension packages a number of code examples from the Core Documentation',
+	'category' => 'example',
+	'author' => 'Core Team',
+	'author_email' => 'info@typo3.org',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"4ef7";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"4fe6";s:20:"doc/wizard_form.html";s:4:"42a4";}',
+);
+
+?>
