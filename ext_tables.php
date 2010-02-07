23b5f572137d4a0ad46bae59df69318b5b447cf1
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-07 14:47:05 +0000

    Added configuration for two sample plugins (no FE code)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/examples/trunk@29770 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -36,12 +36,18 @@ t3lib_extMgm::addToAllTCAtypes('fe_users','tx_examples_options;;;;1-1-1, tx_exam
 	// Load the full TCA
 t3lib_div::loadTCA('tt_content');
 
-	// Disable the display of layout and select_key fields
+	// Disable the display of layout and select_key fields for the plugins
+	// provided by the extension
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key,pages';
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi2'] = 'layout,select_key,pages';
 
 	// Activate the display of the plug-in flexform field and set FlexForm definition
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
-t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:examples/flexform_ds.xml');
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:examples/flexform_ds1.xml');
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi2'] = 'pi_flexform';
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_pi2', 'FILE:EXT:examples/flexform_ds2.xml');
 
+	// Add the plugins to the list of plugins
 t3lib_extMgm::addPlugin(array('LLL:EXT:examples/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY . '_pi1'), 'list_type');
+t3lib_extMgm::addPlugin(array('LLL:EXT:examples/locallang_db.xml:tt_content.list_type_pi2', $_EXTKEY . '_pi2'), 'list_type');
 ?>
