a4cd568a0619ec997560a1ecd4986f93c3c0fb1b
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-20 19:32:04 +0000

    Added second XCLASSing example Moved XCLASS classes to "xclasses" subfolder
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/examples/trunk@31285 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,5 +6,8 @@ if (!defined ('TYPO3_MODE')) {
 }
 
 	// Load XCLASSing of db_new
-$TYPO3_CONF_VARS['BE']['XCLASS']['typo3/db_new.php'] = t3lib_extMgm::extPath($_EXTKEY, 'class.tx_examples_xclass.php');
+$TYPO3_CONF_VARS['BE']['XCLASS']['typo3/db_new.php'] = t3lib_extMgm::extPath($_EXTKEY, 'xclasses/class.tx_examples_scdbnew.php');
+
+	// Load XCLASSing of TCEforms
+$TYPO3_CONF_VARS['BE']['XCLASS']['t3lib/class.t3lib_tceforms.php'] = t3lib_extMgm::extPath($_EXTKEY, 'xclasses/class.tx_examples_tceforms.php');
 ?>
