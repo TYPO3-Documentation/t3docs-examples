47f60f9e5b62ea08440460268ecaf28d3e1c6746
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-21 19:43:02 +0000

    Released to TER as version 0.1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/examples/trunk@31301 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "examples".
 #
-# Auto generated 05-02-2010 16:53
+# Auto generated 21-03-2010 20:17
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.3.0-4.3.99',
@@ -39,7 +39,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"4ef7";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"4fe6";s:20:"doc/wizard_form.html";s:4:"42a4";}',
+	'_md5_values_when_last_written' => 'a:21:{s:9:"ChangeLog";s:4:"94a0";s:10:"README.txt";s:4:"71e5";s:25:"class.tx_examples_tca.php";s:4:"19b8";s:12:"ext_icon.gif";s:4:"18c7";s:17:"ext_localconf.php";s:4:"ff40";s:14:"ext_tables.php";s:4:"dbb7";s:14:"ext_tables.sql";s:4:"646f";s:26:"icon_tx_examples_dummy.gif";s:4:"f3b6";s:26:"icon_tx_examples_haiku.gif";s:4:"737d";s:13:"locallang.xml";s:4:"13b7";s:33:"locallang_csh_txexampleshaiku.xml";s:4:"7fb4";s:16:"locallang_db.xml";s:4:"d77b";s:7:"tca.php";s:4:"9f71";s:14:"doc/manual.sxw";s:4:"16cc";s:26:"flexforms/flexform_ds1.xml";s:4:"28af";s:26:"flexforms/flexform_ds2.xml";s:4:"e9af";s:26:"flexforms/flexform_ds3.xml";s:4:"1011";s:26:"flexforms/flexform_ds4.xml";s:4:"985c";s:30:"res/images/japanese_garden.jpg";s:4:"7e0a";s:38:"xclasses/class.tx_examples_scdbnew.php";s:4:"2ab8";s:39:"xclasses/class.tx_examples_tceforms.php";s:4:"8401";}',
+	'suggests' => array(
+	),
 );
 
 ?>
