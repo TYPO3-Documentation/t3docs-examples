1b4c1fb7f11fa9049be9fcc64138020f19dd6b13
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-30 11:59:11 +0000

    Raised version compatibility for TYPO3 4.5, resolves #30694
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/examples/trunk@53550 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.3.0-4.3.99',
+			'typo3' => '4.3.0-4.5.99',
 		),
 		'conflicts' => array(
 		),
