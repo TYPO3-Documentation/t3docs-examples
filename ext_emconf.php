ad7b574cbf1047e27a242256b722118a53dd4cdd
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-21 19:48:13 +0000

    Released to TER as version 0.3.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/examples/trunk@54547 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "examples".
 #
-# Auto generated 30-10-2011 18:41
+# Auto generated 21-11-2011 20:47
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -14,8 +14,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Core Documentation Code Examples',
 	'description' => 'This extension packages a number of code examples from the Core Documentation',
 	'category' => 'example',
-	'author' => 'TYPO3 Core Development Team',
-	'author_email' => 'info@typo3.org',
+	'author' => 'Documentation Team',
+	'author_email' => 'documentation@typo3.org',
 	'shy' => '',
 	'dependencies' => '',
 	'conflicts' => '',
@@ -29,17 +29,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.2.0',
+	'version' => '0.3.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.3.0-4.5.99',
+			'typo3' => '4.3.0-4.6.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"6468";s:10:"README.txt";s:4:"71e5";s:25:"class.tx_examples_tca.php";s:4:"9698";s:16:"ext_autoload.php";s:4:"6d41";s:12:"ext_icon.gif";s:4:"18c7";s:17:"ext_localconf.php";s:4:"ff40";s:14:"ext_tables.php";s:4:"520b";s:14:"ext_tables.sql";s:4:"c9ab";s:26:"icon_tx_examples_dummy.gif";s:4:"f3b6";s:26:"icon_tx_examples_haiku.gif";s:4:"737d";s:13:"locallang.xml";s:4:"13b7";s:33:"locallang_csh_txexampleshaiku.xml";s:4:"7fb4";s:16:"locallang_db.xml";s:4:"915b";s:7:"tca.php";s:4:"c0d2";s:14:"doc/manual.sxw";s:4:"d907";s:26:"flexforms/flexform_ds1.xml";s:4:"28af";s:26:"flexforms/flexform_ds2.xml";s:4:"e9af";s:26:"flexforms/flexform_ds3.xml";s:4:"1011";s:26:"flexforms/flexform_ds4.xml";s:4:"985c";s:30:"res/images/japanese_garden.jpg";s:4:"7e0a";s:38:"xclasses/class.tx_examples_scdbnew.php";s:4:"2ab8";s:39:"xclasses/class.tx_examples_tceforms.php";s:4:"5a4d";}',
+	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"55d2";s:10:"README.txt";s:4:"71e5";s:25:"class.tx_examples_tca.php";s:4:"4dbb";s:16:"ext_autoload.php";s:4:"6d41";s:12:"ext_icon.gif";s:4:"18c7";s:17:"ext_localconf.php";s:4:"ff40";s:14:"ext_tables.php";s:4:"9608";s:14:"ext_tables.sql";s:4:"f5cc";s:26:"icon_tx_examples_dummy.gif";s:4:"f3b6";s:26:"icon_tx_examples_haiku.gif";s:4:"737d";s:13:"locallang.xml";s:4:"13b7";s:33:"locallang_csh_txexampleshaiku.xml";s:4:"7fb4";s:16:"locallang_db.xml";s:4:"06dd";s:7:"tca.php";s:4:"dd59";s:14:"doc/manual.sxw";s:4:"d907";s:26:"flexforms/flexform_ds1.xml";s:4:"28af";s:26:"flexforms/flexform_ds2.xml";s:4:"e9af";s:26:"flexforms/flexform_ds3.xml";s:4:"1011";s:26:"flexforms/flexform_ds4.xml";s:4:"985c";s:30:"res/images/japanese_garden.jpg";s:4:"7e0a";s:16:"static/setup.txt";s:4:"7528";s:38:"xclasses/class.tx_examples_scdbnew.php";s:4:"2ab8";s:39:"xclasses/class.tx_examples_tceforms.php";s:4:"3c11";}',
 	'suggests' => array(
 	),
 );
