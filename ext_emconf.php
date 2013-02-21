a4526bacfc07f32e2b925ce97f171f6c37f3f5db
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-21 13:49:52 +0000

    Released to TER as version 0.4.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/examples/trunk@71378 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "examples".
-#
-# Auto generated 21-11-2011 20:47
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "examples".
+ *
+ * Auto generated 21-02-2013 14:48
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Core Documentation Code Examples',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.3.0',
+	'version' => '0.4.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.3.0-4.7.99',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"55d2";s:10:"README.txt";s:4:"71e5";s:25:"class.tx_examples_tca.php";s:4:"4dbb";s:16:"ext_autoload.php";s:4:"6d41";s:12:"ext_icon.gif";s:4:"18c7";s:17:"ext_localconf.php";s:4:"ff40";s:14:"ext_tables.php";s:4:"9608";s:14:"ext_tables.sql";s:4:"f5cc";s:26:"icon_tx_examples_dummy.gif";s:4:"f3b6";s:26:"icon_tx_examples_haiku.gif";s:4:"737d";s:13:"locallang.xml";s:4:"13b7";s:33:"locallang_csh_txexampleshaiku.xml";s:4:"7fb4";s:16:"locallang_db.xml";s:4:"06dd";s:7:"tca.php";s:4:"dd59";s:14:"doc/manual.sxw";s:4:"d907";s:26:"flexforms/flexform_ds1.xml";s:4:"28af";s:26:"flexforms/flexform_ds2.xml";s:4:"e9af";s:26:"flexforms/flexform_ds3.xml";s:4:"1011";s:26:"flexforms/flexform_ds4.xml";s:4:"985c";s:30:"res/images/japanese_garden.jpg";s:4:"7e0a";s:16:"static/setup.txt";s:4:"7528";s:38:"xclasses/class.tx_examples_scdbnew.php";s:4:"2ab8";s:39:"xclasses/class.tx_examples_tceforms.php";s:4:"3c11";}',
+	'_md5_values_when_last_written' => 'a:32:{s:9:"ChangeLog";s:4:"870e";s:25:"class.tx_examples_tca.php";s:4:"4dbb";s:16:"ext_autoload.php";s:4:"6d41";s:12:"ext_icon.gif";s:4:"18c7";s:17:"ext_localconf.php";s:4:"9b37";s:14:"ext_tables.php";s:4:"d616";s:14:"ext_tables.sql";s:4:"f5cc";s:26:"icon_tx_examples_dummy.gif";s:4:"f3b6";s:26:"icon_tx_examples_haiku.gif";s:4:"737d";s:13:"locallang.xml";s:4:"13b7";s:33:"locallang_csh_txexampleshaiku.xml";s:4:"7fb4";s:16:"locallang_db.xml";s:4:"06dd";s:10:"README.txt";s:4:"71e5";s:7:"tca.php";s:4:"dd59";s:40:"Classes/Controller/DefaultController.php";s:4:"acd0";s:28:"Classes/ExtDirect/Server.php";s:4:"50d0";s:30:"Classes/Utilities/Viewport.php";s:4:"7838";s:43:"Classes/ViewHelpers/Be/HeaderViewHelper.php";s:4:"b149";s:40:"Resources/Private/Language/locallang.xml";s:4:"8600";s:37:"Resources/Private/Layouts/Module.html";s:4:"6f69";s:46:"Resources/Private/Templates/Default/Flash.html";s:4:"f524";s:38:"Resources/Public/Images/moduleIcon.png";s:4:"671c";s:42:"Resources/Public/JavaScript/Application.js";s:4:"6d2d";s:46:"components/navigation/javascript/Navigation.js";s:4:"624c";s:14:"doc/manual.sxw";s:4:"d907";s:26:"flexforms/flexform_ds1.xml";s:4:"28af";s:26:"flexforms/flexform_ds2.xml";s:4:"e9af";s:26:"flexforms/flexform_ds3.xml";s:4:"1011";s:26:"flexforms/flexform_ds4.xml";s:4:"985c";s:30:"res/images/japanese_garden.jpg";s:4:"7e0a";s:16:"static/setup.txt";s:4:"7528";s:38:"xclasses/class.tx_examples_scdbnew.php";s:4:"cd22";}',
 	'suggests' => array(
 	),
 );
