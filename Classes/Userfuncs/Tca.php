f8c54421f6b81781204e7f2569e116aaf49be06b
Francois Suter <francois@typo3.org>
2016-11-18 10:46:52 +0100

    Updated user-type field TCA example

--- Classes/Userfuncs/Tca.php
+++ Classes/Userfuncs/Tca.php
@@ -14,76 +14,84 @@ namespace Documentation\Examples\Userfuncs;
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3\CMS\Backend\Form\Element\UserElement;
+
 /**
  * Class that implements many examples related to TCA or TCEform manipulations
  *
- * @author Francois Suter <francois@typo3.org>
+ * @author Francois Suter <francois.suter@typo3.org>
  * @package TYPO3
  * @subpackage tx_examples
  */
-class Tca {
+class Tca
+{
 
-	/**
-	 * This method renders a user-defined field
-	 *
-	 * @param	array	$PA: parameters of the field
-	 * @param	object	$fObj: calling object (TCEform)
-	 * @return	string	HTML for the field
-	 */
-	public function specialField($PA, $fObj) {
-		$color = (isset($PA['parameters']['color'])) ? $PA['parameters']['color'] : 'red';
-		$formField  = '<div style="padding: 5px; background-color: ' . $color . ';">';
-		$formField .= '<input type="text" name="' . $PA['itemFormElName'] . '"';
-		$formField .= ' value="' . htmlspecialchars($PA['itemFormElValue']) . '"';
-		$formField .= ' onchange="' . htmlspecialchars(implode('', $PA['fieldChangeFunc'])) . '"';
-		$formField .= $PA['onFocus'];
-		$formField .= ' /></div>';
-		return $formField;
-	}
+    /**
+     * Renders a custom, user-defined field.
+     *
+     * @param array $configuration
+     * @param UserElement $parentObject
+     * @return string HTML for the field
+     */
+    public function specialField($configuration, $parentObject)
+    {
+        $color = (isset($configuration['parameters']['color'])) ? $configuration['parameters']['color'] : 'red';
+        $size = (isset($configuration['parameters']['size'])) ? (int)$configuration['parameters']['size'] : 20;
+        $formField = '<div style="padding: 5px; background-color: ' . $color . ';">';
+        $formField .= '<input type="text" name="' . $configuration['itemFormElName'] . '"';
+        $formField .= ' value="' . htmlspecialchars($configuration['itemFormElValue']) . '"';
+        $formField .= ' size="' . $size . '"';
+        $formField .= ' onchange="' . htmlspecialchars(implode('', $configuration['fieldChangeFunc'])) . '"';
+        $formField .= $configuration['onFocus'];
+        $formField .= ' /></div>';
+        return $formField;
+    }
 
-	/**
-	 * This method renders a wizard providing JavaScript +/- controls
-	 * to increase or decrease an integer value in a field
-	 *
-	 * @param	array	$PA: parameters of the field
-	 * @param	object	$fObj: calling object (TCEform)
-	 * @return	string	HTML for the wizard
-	 */
-	public function someWizard($PA, $fObj) {
-		// Note that the information is passed by reference,
-		// so it's possible to manipulate the field directly
-		// Here we highlight the field with the color passed as parameter
-		$backgroundColor = 'white';
-		if (!empty($PA['params']['color'])) {
-			$backgroundColor = $PA['params']['color'];
-		}
-		$PA['item'] = '<div style="background-color: ' . $backgroundColor . '; padding: 4px;">' . $PA['item'] . '</div>';
+    /**
+     * Renders a wizard providing JavaScript +/- controls
+     * to increase or decrease an integer value in a field.
+     *
+     * @param    array $PA : parameters of the field
+     * @param    object $fObj : calling object (TCEform)
+     * @return    string    HTML for the wizard
+     */
+    public function someWizard($PA, $fObj)
+    {
+        // Note that the information is passed by reference,
+        // so it's possible to manipulate the field directly
+        // Here we highlight the field with the color passed as parameter
+        $backgroundColor = 'white';
+        if (!empty($PA['params']['color'])) {
+            $backgroundColor = $PA['params']['color'];
+        }
+        $PA['item'] = '<div style="background-color: ' . $backgroundColor . '; padding: 4px;">' . $PA['item'] . '</div>';
 
-		// Assemble the wizard itself
-		$output = '<div style="margin-top: 8px; margin-left: 4px;">';
+        // Assemble the wizard itself
+        $output = '<div style="margin-top: 8px; margin-left: 4px;">';
 
-		$commonJavascriptCalls = $PA['fieldChangeFunc']['TBE_EDITOR_fieldChanged'] . $PA['fieldChangeFunc']['typo3form.fieldGet'] . ' return false;';
-		// Create the + button
-		$onClick = "document." . $PA['formName'] . "['" . $PA['itemName'] . "'].value++; " . $commonJavascriptCalls;
-		$output .= '<a href="#" onclick="' . htmlspecialchars($onClick) . '" style="padding: 6px; border: 1px solid black; background-color: #999">+</a>';
-		// Create the - button
-		$onClick = "document." . $PA['formName'] . "['" . $PA['itemName'] . "'].value--; " . $commonJavascriptCalls;
-		$output .= '<a href="#" onclick="' . htmlspecialchars($onClick) . '" style="padding: 6px; border: 1px solid black; background-color: #999">-</a>';
-		$output .= '</div>';
-		return $output;
-	}
+        $commonJavascriptCalls = $PA['fieldChangeFunc']['TBE_EDITOR_fieldChanged'] . $PA['fieldChangeFunc']['typo3form.fieldGet'] . ' return false;';
+        // Create the + button
+        $onClick = "document." . $PA['formName'] . "['" . $PA['itemName'] . "'].value++; " . $commonJavascriptCalls;
+        $output .= '<a href="#" onclick="' . htmlspecialchars($onClick) . '" style="padding: 6px; border: 1px solid black; background-color: #999">+</a>';
+        // Create the - button
+        $onClick = "document." . $PA['formName'] . "['" . $PA['itemName'] . "'].value--; " . $commonJavascriptCalls;
+        $output .= '<a href="#" onclick="' . htmlspecialchars($onClick) . '" style="padding: 6px; border: 1px solid black; background-color: #999">-</a>';
+        $output .= '</div>';
+        return $output;
+    }
 
-	/**
-	 * This method creates custom titles for the records of the tx_examples_haiku table
-	 *
-	 * @param array $parameters Parameters used to identify the current record
-	 * @param object $parentObject Calling object
-	 * @return void
-	 */
-	public function haikuTitle(&$parameters, $parentObject) {
-		$record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($parameters['table'], $parameters['row']['uid']);
-		$newTitle = $record['title'];
-		$newTitle .= ' (' . substr(strip_tags($record['poem']), 0, 10) . '...)';
-		$parameters['title'] = $newTitle;
-	}
+    /**
+     * Creates custom titles for the records of the tx_examples_haiku table
+     *
+     * @param array $parameters Parameters used to identify the current record
+     * @param object $parentObject Calling object
+     * @return void
+     */
+    public function haikuTitle(&$parameters, $parentObject)
+    {
+        $record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($parameters['table'], $parameters['row']['uid']);
+        $newTitle = $record['title'];
+        $newTitle .= ' (' . substr(strip_tags($record['poem']), 0, 10) . '...)';
+        $parameters['title'] = $newTitle;
+    }
 }
--- Configuration/TCA/Overrides/fe_users.php
+++ Configuration/TCA/Overrides/fe_users.php
@@ -3,43 +3,59 @@ defined('TYPO3_MODE') or die();
 
 // Add some fields to FE Users table to show TCA fields definitions
 // USAGE: TCA Reference > $TCA array reference > ['columns'][fieldname]['config'] / TYPE: "select"
-$temporaryColumns = array (
-	'tx_examples_options' => array (
-		'exclude' => 0,
-		'label' => 'LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:fe_users.tx_examples_options',
-		'config' => array (
-			'type' => 'select',
-			'items' => array (
-				array('LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:fe_users.tx_examples_options.I.0', '1'),
-				array('LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:fe_users.tx_examples_options.I.1', '2'),
-				array('LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:fe_users.tx_examples_options.I.2', '--div--'),
-				array('LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:fe_users.tx_examples_options.I.3', '3'),
-			),
-			'size' => 1,
-			'maxitems' => 1,
-		)
-	),
-	'tx_examples_special' => array (
-		'exclude' => 0,
-		'label' => 'LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:fe_users.tx_examples_special',
-		'config' => array (
-			'type' => 'user',
-			'size' => '30',
-			'userFunc' => 'Documentation\\Examples\\Userfuncs\\Tca->specialField',
-			'parameters' => array(
-				'color' => 'blue'
-			)
-		)
-	),
+$temporaryColumns = array(
+        'tx_examples_options' => array(
+                'exclude' => 0,
+                'label' => 'LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:fe_users.tx_examples_options',
+                'config' => array(
+                        'type' => 'select',
+                        'items' => array(
+                                array(
+                                        '',
+                                        0
+                                ),
+                                array(
+                                        'LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:fe_users.tx_examples_options.I.0',
+                                        1
+                                ),
+                                array(
+                                        'LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:fe_users.tx_examples_options.I.1',
+                                        2
+                                ),
+                                array(
+                                        'LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:fe_users.tx_examples_options.I.2',
+                                        '--div--'
+                                ),
+                                array(
+                                        'LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:fe_users.tx_examples_options.I.3',
+                                        3
+                                ),
+                        ),
+                        'size' => 1,
+                        'maxitems' => 1,
+                )
+        ),
+        'tx_examples_special' => array(
+                'exclude' => 0,
+                'label' => 'LLL:EXT:examples/Resources/Private/Language/locallang_db.xlf:fe_users.tx_examples_special',
+                'config' => array(
+                        'type' => 'user',
+                        'userFunc' => \Documentation\Examples\Userfuncs\Tca::class . '->specialField',
+                        'parameters' => array(
+                                'size' => '30',
+                                'color' => 'blue'
+                        )
+                )
+        ),
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
-	'fe_users',
-	$temporaryColumns,
-	TRUE
+        'fe_users',
+        $temporaryColumns,
+        true
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
-	'fe_users',
-	'tx_examples_options;;;;1-1-1, tx_examples_special'
+        'fe_users',
+        'tx_examples_options, tx_examples_special'
 );
