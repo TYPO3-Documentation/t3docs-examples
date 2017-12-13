99ac1220797ce0eb016f95d405e69ae5976faac4
Tymoteusz Motylewski <t.motylewski@gmail.com>
2017-12-13 12:04:43 +0100

    Fix custom navigation component registration

--- components/navigation/javascript/Navigation.js
+++ components/navigation/javascript/Navigation.js
@@ -1,10 +1,24 @@
 Ext.ns('TYPO3.Navigation');
 
 TYPO3.Navigation.Navigator = Ext.extend(Ext.Panel, {
-	id: 'typo3-navigation',
-	html: 'Hello World!'
+  id: 'typo3-navigation',
+  html: 'Hello World!',
 });
 
-TYPO3.ModuleMenu.App.registerNavigationComponent('typo3-navigation', function () {
-	return new TYPO3.Navigation.Navigator();
-});
+/**
+ * Callback method for the module menu
+ *
+ */
+require(
+  [
+    'TYPO3/CMS/Backend/ModuleMenu'
+  ],
+  function () {
+    // extjs loading bugfix
+    window.setTimeout(function() {
+      TYPO3.ModuleMenu.App.registerNavigationComponent('typo3-navigation', function () {
+        return new TYPO3.Navigation.Navigator();
+      });
+    }, 1000);
+  }
+);
