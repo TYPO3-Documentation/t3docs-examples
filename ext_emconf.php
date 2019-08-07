9b7b7ebeed5d523240710f0747f2e021a7541b7d
Sybille Peters <sypets@gmx.de>
2019-08-07 10:58:36 +0200

    Changes for version 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = [
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
     'author_company' => '',
-    'version' => '0.8.0',
+    'version' => '9.5.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '9.5.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
