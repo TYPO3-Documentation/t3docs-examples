0ab988d26e24911251534f256b50538f802305c7
linawolf <48202465+linawolf@users.noreply.github.com>
2020-11-27 20:02:53 +0100

    correct requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '10.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.0.0-10.0.99',
+            'typo3' => '10.4.10-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
