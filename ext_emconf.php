f535c95eaf23f5dd7302df9db7a557227957fdb5
linawolf <48202465+linawolf@users.noreply.github.com>
2021-01-01 10:59:32 +0100

    [TASK] Make master availible for 11

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '10.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.10-10.4.99',
+            'typo3' => '11.0.0-11.0.99',
         ],
         'conflicts' => [],
         'suggests' => [],
