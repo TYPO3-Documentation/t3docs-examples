4e29c4541521e68f3f8f5341b59bc6fe590f8808
linawolf <48202465+linawolf@users.noreply.github.com>
2021-01-02 19:51:54 +0100

    [Feature] Add Custom Data processor
    Add example for a simple Content element to be used in TYPO3 Explained

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -188,6 +188,40 @@ $GLOBALS['TCA']['tt_content']['types']['examples_newcontentcsv'] = [
     ],
 ];
 
+// Adds the content element to the "Type" dropdown
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'LLL:EXT:examples/Resources/Private/Language/locallang.xlf:examples_newcontentelement_title',
+        'examples_newcontentelement',
+        'content-text',
+    ],
+    'textmedia',
+    'after'
+);
+
+// Configure the default backend fields for the content element
+$GLOBALS['TCA']['tt_content']['types']['examples_newcontentelement'] = [
+    'showitem' => '
+           --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+               --palette--;;general,
+               header; Internal title (not displayed),
+               bodytext;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext_formlabel,
+           --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+               --palette--;;hidden,
+               --palette--;;access,
+       ',
+    'columnsOverrides' => [
+        'bodytext' => [
+            'config' => [
+                'enableRichtext' => true,
+                'richtextConfiguration' => 'default',
+            ],
+        ],
+    ],
+];
+
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
     'tt_content',
