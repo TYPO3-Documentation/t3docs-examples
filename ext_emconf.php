e66e5de8b384f00775eafddd616cd3b37bc9642f
lina.wolf <lwolf@w-commerce.de>
2022-02-13 09:04:35 +0100

    [TASK] Make a version for v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = [
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
     'author_company' => '',
-    'version' => '11.1.1',
+    'version' => '12.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.0-11.5.99',
+            'typo3' => '12.0.0-12.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
