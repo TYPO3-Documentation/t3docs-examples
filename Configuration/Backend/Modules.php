ee330fd5e09f6eab9d6cc1dc83ef6e559d851c7a
lina.wolf <lwolf@w-commerce.de>
2022-02-20 11:04:05 +0100

    [FEATURE] use New backend module registration API
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Breaking-96812-NoFrontendTypoScriptBasedTemplateOverridesInTheBackend.html
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Feature-96730-SimplifiedExtbackendModuleTemplateAPI.html
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Feature-96733-NewBackendModuleRegistrationAPI.html#module-dependent-configuration-options
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Breaking-96733-RemovedSupportForModuleHandlingBasedOnTBE_MODULES.html

---
+++ Configuration/Backend/Modules.php
@@ -0,0 +1,38 @@
+<?php
+
+use T3docs\Examples\Controller\ModuleController;
+use T3docs\Examples\Controller\AdminModuleController;
+
+/**
+ * Definitions for modules provided by EXT:examples
+ */
+return [
+    'web_examples' => [
+        'parent' => 'web',
+        'position' => ['after' => 'web_info'],
+        'access' => 'user',
+        'workspaces' => 'live',
+        'path' => '/module/page/example',
+        'labels' => 'LLL:EXT:examples/Resources/Private/Language/Module/locallang_mod.xlf',
+        'extensionName' => 'Examples',
+        'controllerActions' => [
+            ModuleController::class => [
+                'flash','tree','clipboard','links','fileReference','fileReferenceCreate',
+            ],
+        ],
+    ],
+    'admin_examples' => [
+        'parent' => 'system',
+        'position' => ['top'],
+        'access' => 'user',
+        'workspaces' => 'live',
+        'path' => '/module/system/example',
+        'labels' => 'LLL:EXT:examples/Resources/Private/Language/AdminModule/locallang_mod.xlf',
+        'extensionName' => 'Examples',
+        'controllerActions' => [
+            AdminModuleController::class => [
+                'index',
+            ],
+        ],
+    ],
+];
