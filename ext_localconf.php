3e81f82996b0ef290fd05cb8e5965f0325f2d2cd
stanschus <stanschus@gmx.de>
2022-03-02 17:49:34 +0100

    [BUGFIX] correct Handling of custom github link in LinkBrowser (RTE) (#70)
    * [BUGFIX] correct Handling of custom github link in LinkBrowser (RTE)
    new Class: T3docs\Examples\LinkHandling\GithubLinkhandler
    so TYPO3\CMS\Core\LinkHandling\LinkService will recognize github: and build an array with ['url']['github] in it, which is needed by T3docs\Examples\LinkHandler\GithubLinkhandler->canHandleLink to get triggered.
    
    * [BUGFIX] fixed a possible override if multiple LinkHandlers were registered through $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['Link']['resolveByStringRepresentation']

--- ext_localconf.php
+++ ext_localconf.php
@@ -144,3 +144,7 @@ TCEMAIN.linkHandler {
 }
 ');
 
+# not sure about this one because it doesn't get involved somehow
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['linkHandler']['github'] = \T3docs\Examples\LinkHandling\GithubLinkHandler::class;
+# this one is crucial
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['Link']['resolveByStringRepresentation'][] = 'T3docs\Examples\LinkHandling\GithubLinkHandler->resolveHandlerData';
