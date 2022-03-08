94a4409a135746b086212532ed9793fab5ec0c9d
lina.wolf <lwolf@w-commerce.de>
2022-03-08 14:32:48 +0100

    [DOCS] Custom Linkvalidator type
    refs https://forge.typo3.org/issues/96237

---
+++ Classes/LinkValidator/LinkType/ExampleLinkType.php
@@ -0,0 +1,46 @@
+<?php
+namespace  T3docs\Examples\LinkValidator\LinkType;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Linkvalidator\Linktype\AbstractLinktype;
+
+/**
+ * This class provides Check Example Links plugin implementation
+ */
+class ExampleLinkType extends AbstractLinktype
+{
+    protected string $identifier = 'example';
+
+    public function checkLink($url, $softRefEntry, $reference)
+    {
+        $isValidUrl = false;
+        // TODO: Implement checkLink() method.
+        return $isValidUrl;
+    }
+
+    public function getErrorMessage($errorParams)
+    {
+        $lang = $this->getLanguageService();
+        switch ($errorParams['errno'] ?? 0) {
+            case 404:
+                $message = $lang->getLL('list.report.pagenotfound404');
+                break;
+            default:
+                // fall back to generic error message
+                $message = sprintf($lang->getLL('list.report.externalerror'), $errorParams['errno']);
+        }
+        return $message;
+    }
+}
