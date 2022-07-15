5162cfcc00754a1bdb746cfd327ebf3ea66eba1e
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2022-07-15 10:55:31 +0200

    [FEATURE] Example for a non-extbase backend module in new API (#71)
    * [FEATURE] Example for a non-extbase backend module in new API
    
    * Switch to proper guilables, correct case of names of modules (#1914)
    
    * Switch to proper guilables, correct case of names of modules
    
    * Update Documentation/ApiOverview/ErrorAndExceptionHandling/ProductionExceptionHandler/Index.rst
    
    Co-authored-by: lina.wolf <lwolf@w-commerce.de>
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    (cherry picked from commit 75a5e910e7d47c165d859ce1521af3a070752c6d)
    
    Co-authored-by: lina.wolf <lwolf@w-commerce.de>

--- Classes/Controller/AdminModuleController.php
+++ Classes/Controller/AdminModuleController.php
@@ -2,20 +2,115 @@
 namespace T3docs\Examples\Controller;
 
 use Psr\Http\Message\ResponseInterface;
-use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Backend\Template\Components\ButtonBar;
+use TYPO3\CMS\Backend\Template\ModuleTemplate;
 use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
+use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Localization\LanguageService;
 
-class AdminModuleController extends ActionController {
+class AdminModuleController
+{
 
     public function __construct(
         protected readonly ModuleTemplateFactory $moduleTemplateFactory,
-    ) {
+        protected readonly IconFactory $iconFactory,
+        // ...
+    ) {}
+
+    public function handleRequest(ServerRequestInterface $request): ResponseInterface
+    {
+        $GLOBALS['LANG']->includeLLFile('EXT:examples/Resources/Private/Language/AdminModule/locallang.xlf');
+
+        $allowedOptions = [
+            'function' => [
+                'debug' => $GLOBALS['LANG']->getLL('debug'),
+                'password' => $GLOBALS['LANG']->getLL('password'),
+                'index' => $GLOBALS['LANG']->getLL('index'),
+            ],
+        ];
+
+        $moduleData = $request->getAttribute('moduleData');
+        if ($moduleData->cleanUp($allowedOptions)) {
+            $GLOBALS['BE_USER']->pushModuleData($moduleData->getModuleIdentifier(), $moduleData->toArray());
+        }
+
+        $moduleTemplate = $this->moduleTemplateFactory->create($request, 't3docs/examples');
+        $this->setUpDocHeader($request, $moduleTemplate);
+
+        $title = $GLOBALS['LANG']->sL('LLL:EXT:examples/Resources/Private/Language/AdminModule/locallang_mod.xlf:mlang_tabs_tab');
+        switch ($moduleData->get('function')) {
+            case 'debug':
+                $moduleTemplate->setTitle($title, $GLOBALS['LANG']->getLL('module.menu.debug'));
+                return $this->debugAction($request, $moduleTemplate);
+            case 'password':
+                $moduleTemplate->setTitle($title, $GLOBALS['LANG']->getLL('module.menu.password'));
+                return $this->passwordAction($request, $moduleTemplate);
+            default:
+                $moduleTemplate->setTitle($title, $GLOBALS['LANG']->getLL('module.menu.log'));
+                return $this->indexAction($request, $moduleTemplate);
+        }
     }
 
-    public function indexAction(): ResponseInterface
+    private function setUpDocHeader(
+        ServerRequestInterface $request,
+        ModuleTemplate $view
+    ) {
+        $buttonBar = $view->getDocHeaderComponent()->getButtonBar();
+        $list = $buttonBar->makeLinkButton()
+            ->setHref('<uri-builder-path>')
+            ->setTitle('A Title')
+            ->setShowLabelText('Link')
+            ->setIcon($this->iconFactory->getIcon('actions-extension-import', Icon::SIZE_SMALL));
+        $buttonBar->addButton($list, ButtonBar::BUTTON_POSITION_LEFT, 1);
+    }
+
+    public function indexAction(
+        ServerRequestInterface $request,
+        ModuleTemplate $view
+    ) : ResponseInterface
     {
-        $moduleTemplate = $this->moduleTemplateFactory->create($this->request);
-        $moduleTemplate->assign('aVariable', 'aValue');
-        return $moduleTemplate->renderResponse();
+        $view->assign('aVariable', 'aValue');
+        return $view->renderResponse('AdminModule/Index');
+    }
+
+
+    protected function debugAction(
+        ServerRequestInterface $request,
+        ModuleTemplate $view
+    ): ResponseInterface
+    {
+        $cmd = $request->getParsedBody()['tx_examples_admin_examples']['cmd'] ?? 'cookies';
+        switch ($cmd) {
+            case 'cookies':
+                $this->debugCookies();
+                break;
+            default:
+                // do something else
+        }
+
+        $view->assignMultiple(
+            [
+                'cookies' => $request->getCookieParams(),
+                'lastcommand' => $cmd,
+            ]
+        );
+        return $view->renderResponse('AdminModule/Debug');
+    }
+
+
+    protected function passwordAction(
+        ServerRequestInterface $request,
+        ModuleTemplate $view
+    ): ResponseInterface
+    {
+        // TODO: Do something
+        return $view->renderResponse('AdminModule/Password');
+    }
+
+    private function debugCookies()
+    {
+        // TODO: do something()
     }
 }
