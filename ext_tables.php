5e1136dbfa7784c83151b4c090d492229931b8f2
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-08-12 13:08:03 +0200

    [TASK] Adjust embedding of user TSconfig (#99)
    With #e78aaee17 the Configuration/TSconfig folder was renamed
    to Configuration/TsConfig. Now, the path is corrected when
    importing user TSconfig. Also the extension of the imported file
    was adjusted to tsconfig.
    
    Additionally, a superfluous import of page TSconfig was removed.
    It was previously also available in ext_localconf.php before commit
    #e78aaee17.

--- ext_tables.php
+++ ext_tables.php
@@ -36,11 +36,8 @@ defined('TYPO3') or die();
         'allowedTables' => '*',
     ];
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        "@import 'EXT:examples/Configuration/TSconfig/Page/*.typoscript'"
-    );
     // Add custom doktype to the page tree toolbar
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-        "@import 'EXT:examples/Configuration/TSconfig/User/*.typoscript'"
+        "@import 'EXT:examples/Configuration/TsConfig/User/*.tsconfig'"
     );
 })();
