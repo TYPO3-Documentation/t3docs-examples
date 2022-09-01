4231d35c1b759046a9c6746a6b80f6dcaa08844a
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-09-01 19:59:28 +0200

    [BUGFIX] Remove duplicate adding of TypoScript
    The `addStaticFile` call on `Configuration/TypoScript/`
    is called in `tt_content.php` and `sys_template.php`.
    Therefore, one is removed.

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -2,14 +2,13 @@
 
 defined('TYPO3') or die();
 
-// Declare static TS file
+// Declare static TS files
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'examples',
     'Configuration/TypoScript/',
     'Examples TypoScript'
 );
 
-// Declare static TS file
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'examples',
     'Configuration/TypoScript/HmenuSpecial/',
