860151646ce5aa45424ff8958d3b604895c29bc2
lina.wolf <lwolf@w-commerce.de>
2022-09-18 08:59:24 +0200

    Make CGL happy

--- Classes/Command/CreateWizardCommand.php
+++ Classes/Command/CreateWizardCommand.php
@@ -5,16 +5,15 @@ declare(strict_types=1);
 namespace T3docs\Examples\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use T3docs\Examples\Exception\InvalidWizardException;
 
 class CreateWizardCommand extends Command
 {
-
     protected function configure(): void
     {
         $this
