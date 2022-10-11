0693cbe455c4404a2e427c42cf53485211867e8c
lina.wolf <lwolf@w-commerce.de>
2022-10-11 19:24:34 +0000

    [TASK] Fix Content Menu Icon

--- Resources/Public/JavaScript/ContextMenuActions.js
+++ Resources/Public/JavaScript/ContextMenuActions.js
@@ -1,31 +1,17 @@
 /**
- * Module: TYPO3/CMS/Example/ContextMenuActions
+ * Module: @t3docs/examples/ContextMenuActions
  *
  * JavaScript to handle the click action of the "Hello World" context menu item
- * @exports TYPO3/CMS/Example/ContextMenuActions
  */
-define(function () {
-    'use strict';
 
-    /**
-     * @exports TYPO3/CMS/Example/ContextMenuActions
-     */
-    var ContextMenuActions = {};
+export class ContextMenuActions {
 
-    /**
-     * Say hello
-     *
-     * @param {string} table
-     * @param {int} uid of the page
-     */
-    ContextMenuActions.helloWorld = function (table, uid) {
-        if (table === 'pages') {
-            //If needed, you can access other 'data' attributes here from $(this).data('someKey')
-            //see item provider getAdditionalAttributes method to see how to pass custom data attributes
-
-            top.TYPO3.Notification.error('Hello World', 'Hi there!', 5);
-        }
-    };
-
-    return ContextMenuActions;
-});
+	helloWorld(table, uid) {
+		if (table === 'pages') {
+			//If needed, you can access other 'data' attributes here from $(this).data('someKey')
+			//see item provider getAdditionalAttributes method to see how to pass custom data attributes
+			alert('hello');
+			top.TYPO3.Notification.error('Hello World', 'Hi there!', 5);
+		}
+	};
+}
