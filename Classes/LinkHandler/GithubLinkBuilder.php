39dd8cf33171c0ad7b8d2cc2bb90ab0927482650
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2022-10-13 19:08:35 +0200

    [Forwardport] [BUGFIX] Correct GitHub Linkhandler (#133)
    * [BUGFIX] [PREPORT] Fix the Github Linkhandler for TYPO3 v11
    
    releases: main
    
    * [BUGFIX] [PREPORT] Fix the Github Linkhandler for TYPO3 v11
    
    releases: main
    
    * Happyfy CI
    
    * Apply suggestions from code review
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    * Update Classes/LinkHandler/GithubLinkBuilder.php
    
    * Update CHANGELOG.md
    
    * Add changelog entry
    
    * Use single backslash for FQCN
    
    Co-authored-by: lina.wolf <lwolf@w-commerce.de>
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>

---
+++ Classes/LinkHandler/GithubLinkBuilder.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace T3docs\Examples\LinkHandler;
+
+use TYPO3\CMS\Frontend\Typolink\AbstractTypolinkBuilder;
+use TYPO3\CMS\Frontend\Typolink\LinkResult;
+use TYPO3\CMS\Frontend\Typolink\LinkResultInterface;
+use TYPO3\CMS\Frontend\Typolink\UnableToLinkException;
+
+/**
+ * Builds a TypoLink to a Github issue
+ */
+class GithubLinkBuilder extends AbstractTypolinkBuilder
+{
+    const TYPE_GITHUB = 'github';
+
+    public function build(
+        array &$linkDetails,
+        string $linkText,
+        string $target,
+        array $conf
+    ): LinkResultInterface {
+        $issueId = trim($linkDetails['value']);
+        if (str_starts_with($issueId, '#')) {
+            $issueId = substr($issueId, 1);
+        }
+        $issueId = (int)$issueId;
+        if ($issueId < 1) {
+            throw new UnableToLinkException(
+                '"' . $linkDetails['value'] . '" is not a valid GitHub issue number.',
+                // Use the Unix timestamp of the time of creation of this message
+                1665304602,
+                null,
+                $linkText
+            );
+        }
+        $url = 'https://github.com/TYPO3-Documentation/TYPO3CMS-Reference-CoreApi/issues/' . $issueId;
+
+        return (new LinkResult(self::TYPE_GITHUB, $url))
+            ->withTarget($target)
+            ->withLinkConfiguration($conf)
+            ->withLinkText($linkText);
+    }
+}
