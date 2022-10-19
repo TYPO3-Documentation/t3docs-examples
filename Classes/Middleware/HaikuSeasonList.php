c34ee3e9bc4e9dfb4d8a2d265633205e13159b3d
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-10-19 18:47:51 +0200

    Add header comment

--- Classes/Middleware/HaikuSeasonList.php
+++ Classes/Middleware/HaikuSeasonList.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the "examples" extension for TYPO3.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
 namespace T3docs\Examples\Middleware;
 
 use Psr\Http\Message\ResponseFactoryInterface;
