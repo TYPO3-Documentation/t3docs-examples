c4ef0c664e6117021e9dc586f6e11d33665672a4
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-06-20 19:43:51 +0200

    [FEATURE] Add example for a custom reaction type (#188)
    The incoming webhook retrieves an id which is stored in the registry.
    
    The full scenario for the EXT:reactions docs:
    
    1. Retrieve an id (which may be changed product id from some third-party system)
    2. Store it in the registry.
    3. A command (started regularly by a cronjob) can pick it up and synchronize that changed product ids (which may be time-consuming, therefore not done in the incoming request).

---
+++ Configuration/TCA/Overrides/sys_reaction.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+defined('TYPO3') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'sys_reaction',
+    'reaction_type',
+    [
+        'label' => \T3docs\Examples\Reaction\ExampleReactionType::getDescription(),
+        'value' => \T3docs\Examples\Reaction\ExampleReactionType::getType(),
+        'icon' => \T3docs\Examples\Reaction\ExampleReactionType::getIconIdentifier(),
+    ]
+);
