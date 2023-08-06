af057342fbe794b8003d0b11cc434299653019df
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-08-06 09:06:08 +0200

    [TASK] Adjust TYPO3 version constraints (#195)

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,9 +27,9 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '12.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.99.99',
-            'fluid_styled_content' => '12.0.0-12.99.99',
-            'linkvalidator' => '12.0.0-12.99.99',
+            'typo3' => '12.4.0-12.4.99',
+            'fluid_styled_content' => '12.4.0-12.4.99',
+            'linkvalidator' => '12.4.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
