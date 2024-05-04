7cc731d699e7c22aebe077f5f668ad21c387f085
lina.wolf <lwolf@w-commerce.de>
2024-05-04 09:37:51 +0200

    [TASK] Raise phpstan level to 2
    Fix error in module controller, there is no findall method in the file repository.

--- Classes/Controller/ModuleController.php
+++ Classes/Controller/ModuleController.php
@@ -158,7 +158,7 @@ class ModuleController extends ActionController implements LoggerAwareInterface
         );
 
         // Create and initialize the tree object
-        /** @var $tree PageTreeView */
+        /** @var PageTreeView $tree */
         $tree = GeneralUtility::makeInstance(PageTreeView::class);
         $tree->init('AND ' . $GLOBALS['BE_USER']->getPagePermsClause(1));
         $html = '';
@@ -248,7 +248,7 @@ class ModuleController extends ActionController implements LoggerAwareInterface
      */
     protected function getClipboard(string $identifier): array
     {
-        /** @var $clipboard Clipboard */
+        /** @var Clipboard $clipboard */
         $clipboard = GeneralUtility::makeInstance(Clipboard::class);
         // Read the clipboard content from the user session
         $clipboard->initializeClipboard();
@@ -266,7 +266,7 @@ class ModuleController extends ActionController implements LoggerAwareInterface
      */
     protected function getCurrentClipboard(): array
     {
-        /** @var $clipboard Clipboard */
+        /** @var Clipboard $clipboard */
         $clipboard = GeneralUtility::makeInstance(Clipboard::class);
         // Read the clipboard content from the user session
         $clipboard->initializeClipboard();
@@ -283,7 +283,7 @@ class ModuleController extends ActionController implements LoggerAwareInterface
      */
     protected function debugClipboard()
     {
-        /** @var $clipboard Clipboard */
+        /** @var Clipboard $clipboard */
         $clipboard = GeneralUtility::makeInstance(Clipboard::class);
         // Read the clipboard content from the user session
         $clipboard->initializeClipboard();
@@ -368,7 +368,7 @@ class ModuleController extends ActionController implements LoggerAwareInterface
      *
      * @param int $element Uid of the just processed content element (see fileReferenceCreateAction)
      */
-    public function fileReferenceAction($element = 0): ResponseInterface
+    public function fileReferenceAction(int $element = 0): ResponseInterface
     {
         // Get all non-deleted content elements (this should normally be put away in a nice, clean
         // repository class; don't do this at home).
@@ -385,11 +385,12 @@ class ModuleController extends ActionController implements LoggerAwareInterface
         } catch (\Exception) {
             $contentElements = [];
         }
+        $files = [];
         // If we just handled a content element, get related data to display as a confirmation
-        if ((int)$element > 0) {
+        if ($element > 0) {
             $contentElement = BackendUtility::getRecord(
                 'tt_content',
-                (int)$element,
+                $element,
             );
             try {
                 $fileObjects = $this->fileRepository->findByRelation(
@@ -397,6 +398,7 @@ class ModuleController extends ActionController implements LoggerAwareInterface
                     'image',
                     $element,
                 );
+                $files = $this->fileRepository->findByRelation('tt_content', 'image', $element);
             } catch (\Exception) {
                 $fileObjects = [];
             }
@@ -407,7 +409,7 @@ class ModuleController extends ActionController implements LoggerAwareInterface
         $view = $this->initializeModuleTemplate($this->request);
         $view->assignMultiple(
             [
-                'files' => $this->fileRepository->findAll(),
+                'files' => $files,
                 'elements' => $contentElements,
                 'content' => $contentElement,
                 'references' => $fileObjects,
