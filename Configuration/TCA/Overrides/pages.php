6a3c19b00045a2cbac8d92f9ad61c28dffa81121
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2026-05-17 10:11:59 +0200

    [FEATURE] TCA option allowedRecordTypes for Page Types (#336)
    * [FEATURE] TCA option allowedRecordTypes for Page Types
    
    References https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/1543
    References: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/1642
    
    * [FEATURE] TCA option allowedRecordTypes for Page Types
    
    References https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/1543
    References: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/1642

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -2,19 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 /*
@@ -72,16 +59,3 @@ ExtensionManagementUtility::addFieldsToPalette(
     '--linebreak--,tx_examples_related_pages',
     'after:media',
 );
-
-// SAME as registered in ext_tables.php
-$customPageDoktype = 116;
-$customIconClass = 'tx_examples-archive-page';
-
-// Add the new doktype to the page type selector
-$GLOBALS['TCA']['pages']['columns']['doktype']['config']['items'][] = [
-    'LLL:examples.messages:archive_page_type',
-    $customPageDoktype,
-    $customIconClass,
-];
-// Add the icon to the icon class configuration
-$GLOBALS['TCA']['pages']['ctrl']['typeicon_classes'][$customPageDoktype] = $customIconClass;
